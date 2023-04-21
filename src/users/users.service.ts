7dd51315bc4cdc11bd66154470d70b0c289dc534
User <girain3181@gmail.com>
2023-04-21 04:10:05 +0300

    Initial commit

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/users.dto';
+import { PrismaService } from '../core/orm/prisma.service';
 
 @Injectable()
 export class UsersService {
   private users: any = [];
-
+  constructor(private readonly prismaService: PrismaService) {}
   async createUser(userData: CreateUserDto): Promise<any> {
+    await this.prismaService.user.create({
+      data: {
+        age: userData.age,
+        status: userData.status,
+        name: userData.name,
+        city: userData.city,
+        email: userData.email,
+      },
+    });
     this.users.push(userData);
     return this.users;
   }
